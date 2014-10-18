36556802e95a6115a292e455fc03b624062ada3b
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-10-18 15:46:56 +0400

    Made real script

--- quota_applet/getquota.sh
+++ quota_applet/getquota.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-echo 200000 250000 50000
+# USED QUOTA_TOTAL FREE_on_HOME
+FREE=`df ${HOME} -k|tail -n 1|awk '{print $4}'`
+QUOTA=`quota -w |grep homes|awk '{print $2,$3}'`
+echo ${QUOTA} ${FREE}
