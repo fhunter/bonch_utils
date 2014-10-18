953b428ff649b47d2ae41bbd2107449454f01adb
Nikolaj Zamotaev <fhunter@fhunter.mine.nu>
2014-10-19 02:52:44 +0400

    Added fallback in case there is no quota on ${HOME}

--- quota_applet/getquota.sh
+++ quota_applet/getquota.sh
@@ -2,4 +2,7 @@
 # USED QUOTA_TOTAL FREE_on_HOME
 FREE=`df ${HOME} -k|tail -n 1|awk '{print $4}'`
 QUOTA=`quota -w |grep homes|awk '{print $2,$3}'`
+if [ "x${QUOTA}" = "x" ]; then
+QUOTA=`df ${HOME} -k|tail -n 1|awk '{print $3,$2}'`
+fi
 echo ${QUOTA} ${FREE}
